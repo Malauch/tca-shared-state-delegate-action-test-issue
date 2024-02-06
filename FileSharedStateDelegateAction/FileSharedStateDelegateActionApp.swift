c76e65ffdde9d5d03c97b17f05070600c75efbd0
Michał Jurewicz <malauch@emtb.pl>
2024-02-06 11:54:22 +0100

    Initial Commit

---
+++ FileSharedStateDelegateAction/FileSharedStateDelegateActionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileSharedStateDelegateActionApp.swift
+//  FileSharedStateDelegateAction
+//
+//  Created by Michał Jurewicz on 06/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct FileSharedStateDelegateActionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
